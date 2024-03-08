bc31e14806aa7adf9b680e2861f96072a19af3c0
Lucas Teles <lucas.teles@outlook.com>
2024-03-08 13:21:42 -0300

    WIP

---
+++ samples/LobbyServer/Models.cs
@@ -0,0 +1,91 @@
+﻿namespace LobbyServer;
+
+using PeerToken = Guid;
+using PeerId = Guid;
+
+public enum PeerMode : byte
+{
+    Player,
+    Spectator,
+}
+
+public sealed record PeerEndpoint(string IP, int Port);
+
+public sealed class Peer(string username, PeerEndpoint endpoint, bool ready = false)
+{
+    public PeerId PeerId { get; } = Guid.NewGuid();
+    public string Username { get; } = username;
+
+    public PeerEndpoint Endpoint { get; } = endpoint;
+    public bool Ready { get; private set; } = ready;
+
+    public void ToggleReady() => Ready = !Ready;
+}
+
+public sealed record LobbyEntry(Peer Peer, PeerMode Mode)
+{
+    public PeerToken Token { get; } = PeerToken.NewGuid();
+}
+
+public sealed record SpectatorMapping(PeerId Host, IEnumerable<PeerId> Endpoints);
+
+public sealed class Lobby(string name, DateTimeOffset createdAt)
+{
+    public static readonly TimeSpan DefaultExpiration = TimeSpan.FromMinutes(5);
+    const int MaxPlayers = 4;
+
+    readonly HashSet<LobbyEntry> entries = [];
+    readonly object locker = new();
+
+    public string Name { get; } = name;
+    public DateTimeOffset CreatedAt { get; } = createdAt;
+    public DateTimeOffset ExpiresAt => CreatedAt + DefaultExpiration;
+
+    public IEnumerable<Peer> Players =>
+        entries.Where(x => x.Mode is PeerMode.Player).Take(MaxPlayers).Select(x => x.Peer);
+
+    public IEnumerable<Peer> Spectators =>
+        entries.Where(x => x.Mode is PeerMode.Spectator).Select(x => x.Peer);
+
+    public bool Ready => Players.Count() > 1 && Players.All(p => p.Ready);
+
+    public IEnumerable<SpectatorMapping> SpectatorMapping
+    {
+        get
+        {
+            if (!Ready) return [];
+            return Players.Select((p, playerIndex) => new SpectatorMapping(
+                p.PeerId,
+                Spectators
+                    .Select(x => x.PeerId)
+                    .Where((_, specIndex) => specIndex % playerIndex is 0)
+            ));
+        }
+    }
+
+    public bool HasUserName(string username)
+    {
+        lock (locker)
+            return entries
+                .Any(p => p.Peer.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void AddPeer(LobbyEntry entry)
+    {
+        lock (locker)
+        {
+            if (Ready) return;
+            if (Players.Count() >= MaxPlayers)
+                entry = entry with {Mode = PeerMode.Spectator};
+
+            entries.Add(entry);
+        }
+    }
+
+    public Peer? GetPlayer(PeerToken token) =>
+        entries.SingleOrDefault(x => x.Mode is PeerMode.Player && x.Token == token)?.Peer;
+}
+
+public sealed record LobbyRequest(string LobbyName, int Port, string UserName, PeerMode Mode);
+
+public sealed record LobbyResponse(Lobby Lobby, Guid Token);
---
+++ samples/LobbyServer/Program.cs
@@ -0,0 +1,64 @@
+using System.Net;
+using System.Text.Json.Serialization;
+using LobbyServer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Caching.Memory;
+using static Microsoft.AspNetCore.Http.TypedResults;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddEndpointsApiExplorer()
+    .ConfigureHttpJsonOptions(options => options
+        .SerializerOptions.Converters.Add(new JsonStringEnumConverter()))
+    .Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(o => o // For Swagger
+        .JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
+    .AddSwaggerGen(options => options.SupportNonNullableReferenceTypes())
+    .AddMemoryCache()
+    .AddSingleton(TimeProvider.System);
+
+var app = builder.Build();
+app.UseSwagger().UseSwaggerUI();
+
+app.MapGet("lobby/{name}", Results<Ok<Lobby>, NotFound> (IMemoryCache cache, string name) =>
+    cache.Get<Lobby>(name.ToLower()) is { } lobby ? Ok(lobby) : NotFound());
+
+app.MapPost("lobby", Results<Ok<LobbyResponse>, BadRequest, Conflict, UnprocessableEntity> (
+    HttpContext context, TimeProvider time, IMemoryCache cache, LobbyRequest req) =>
+{
+    if (string.IsNullOrWhiteSpace(req.LobbyName)
+        || req.Port <= IPEndPoint.MinPort
+        || req.Port > IPEndPoint.MaxPort
+        || context.Connection.RemoteIpAddress is not { } userIp)
+        return BadRequest();
+
+    var name = req.LobbyName.Trim().ToLower();
+    var lobby = cache.GetOrCreate(name, e =>
+    {
+        e.SetSlidingExpiration(Lobby.DefaultExpiration);
+        return new Lobby(name, time.GetUtcNow());
+    });
+
+    if (lobby is null || lobby.Ready) return UnprocessableEntity();
+    if (lobby.HasUserName(req.UserName)) return Conflict();
+
+    PeerEndpoint endpoint = new(userIp.ToString(), req.Port);
+    Peer peer = new(req.UserName, endpoint);
+    LobbyEntry entry = new(peer, req.Mode);
+    lobby.AddPeer(entry);
+
+    return Ok(new LobbyResponse(lobby, entry.Token));
+});
+
+app.MapPut("lobby/{id}/ready/{token}", Results<NoContent, NotFound, BadRequest> (
+    IMemoryCache cache, string id, Guid token
+) =>
+{
+    if (string.IsNullOrWhiteSpace(id)) return BadRequest();
+    if (cache.Get<Lobby>(id.ToLower()) is not { } lobby || lobby.GetPlayer(token) is not { } player)
+        return NotFound();
+
+    player.ToggleReady();
+    return NoContent();
+});
+
+app.Run();
