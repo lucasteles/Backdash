832862d84e8d7edc1cb4b5c6bd447ebbf4e022c7
Lucas Teles <lucas.teles@outlook.com>
2024-03-18 17:52:15 -0300

    WIP

--- src/Backdash/Network/Endianness.cs
+++ src/Backdash/Network/Endianness.cs
@@ -1,12 +1,21 @@
 namespace Backdash.Network;
+
+/// <summary>
+/// Defines a endianness value
+/// </summary>
 public enum Endianness : byte
 {
+    /// <summary>Little endian byte order</summary>
     LittleEndian,
+
+    /// <summary>Big endian byte order</summary>
     BigEndian,
 }
+
 static class Platform
 {
     public static readonly Endianness Endianness =
         BitConverter.IsLittleEndian ? Endianness.LittleEndian : Endianness.BigEndian;
+
     public static Endianness GetEndianness(bool network) => network ? Endianness.BigEndian : Endianness;
 }
