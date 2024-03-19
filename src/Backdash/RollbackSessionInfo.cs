56d12771114e2f89c4f5a00c8070a481770dec1d
Lucas Teles <lucas.teles@outlook.com>
2024-03-18 21:37:51 -0300

    WIP

--- src/Backdash/RollbackSessionInfo.cs
+++ src/Backdash/RollbackSessionInfo.cs
@@ -1,21 +1,54 @@
 using Backdash.Data;
+
 namespace Backdash;
+
+/// <summary>
+/// Holds current session network stats
+/// </summary>
 public sealed class RollbackNetworkStatus
 {
+    /// <summary>Current roundtrip ping time</summary>
     public TimeSpan Ping { get; internal set; }
+
+    /// <summary>Remote frame advantage</summary>
     public FrameSpan LocalFramesBehind { get; internal set; }
+
+    /// <summary>Local frame advantage</summary>
     public FrameSpan RemoteFramesBehind { get; internal set; }
+
+    /// <summary>Number of pending queued inputs</summary>
     public int PendingInputCount { get; internal set; }
+
+    /// <summary>Last acknowledged frame</summary>
     public Frame LastAckedFrame { get; internal set; }
+
+    /// <summary>Packages sent info</summary>
     public PackagesInfo Send { get; } = new();
+
+    /// <summary>Packages received info</summary>
     public PackagesInfo Received { get; } = new();
+
+    /// <summary>
+    /// Hold package traffic data
+    /// </summary>
     public sealed class PackagesInfo
     {
+        /// <summary>Last package time</summary>
         public TimeSpan LastTime { get; internal set; }
+
+        /// <summary>Total transferred bytes</summary>
         public ByteSize TotalBytes { get; internal set; }
+
+        /// <summary>Number of packages</summary>
         public int Count { get; internal set; }
+
+        /// <summary>Packages per second</summary>
         public float PackagesPerSecond { get; internal set; }
+
+        /// <summary>Last package frame</summary>
         public Frame LastFrame { get; internal set; }
+
+        /// <summary>Total used bandwidth</summary>
         public ByteSize Bandwidth { get; internal set; }
     }
 }
