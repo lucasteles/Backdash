bdf8acb590cbdf8c160b908c855c7aad5bb5f73d
Lucas Teles <lucas.teles@outlook.com>
2024-03-18 21:12:54 -0300

    WIP

--- src/Backdash/Serialization/BinarySerializer.cs
+++ src/Backdash/Serialization/BinarySerializer.cs
@@ -1,20 +1,64 @@
 using Backdash.Network;
 using Backdash.Serialization.Buffer;
+
 namespace Backdash.Serialization;
+
+/// <summary>
+/// Binary reader for <typeparamref name="T"/>
+/// </summary>
+/// <typeparam name="T">Type to be deserialized.</typeparam>
 public interface IBinaryReader<T>
 {
+    /// <summary>
+    /// Deserialize <paramref name="data"/> into <paramref name="value"/>
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="value"></param>
     int Deserialize(ReadOnlySpan<byte> data, ref T value);
 }
+
+/// <summary>
+/// Binary writer for <typeparamref name="T"/>
+/// </summary>
+/// <typeparam name="T">Type to be serialized.</typeparam>
 public interface IBinaryWriter<T>
 {
+    /// <summary>
+    /// Serialize <paramref name="data"/> into <paramref name="buffer"/>
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="buffer"></param>
     int Serialize(in T data, Span<byte> buffer);
 }
+
+/// <summary>
+/// Binary serializer for <typeparamref name="T"/>
+/// </summary>
+/// <typeparam name="T">Type to be serialized.</typeparam>
 public interface IBinarySerializer<T> : IBinaryReader<T>, IBinaryWriter<T>;
+
+/// <inheritdoc />
 public abstract class BinarySerializer<T> : IBinarySerializer<T>
 {
+    /// <summary>
+    /// Force usage of <see cref="Endianness.BigEndian"/>
+    /// </summary>
     public bool Network { get; init; } = true;
-    protected abstract void Serialize(in BinarySpanWriter writer, in T data);
-    protected abstract void Deserialize(in BinarySpanReader reader, ref T result);
+
+    /// <summary>
+    /// Serialize <paramref name="data"/> using <see cref="BinarySpanWriter"/>
+    /// </summary>
+    /// <param name="binaryWriter">Binary writer</param>
+    /// <param name="data">Data to be written</param>
+    protected abstract void Serialize(in BinarySpanWriter binaryWriter, in T data);
+
+    /// <summary>
+    /// Deserialize buffer data using <paramref name="binaryReader"/> into <paramref name="result"/>
+    /// </summary>
+    /// <param name="binaryReader">Binary reader</param>
+    /// <param name="result">Reference to be set with the deserialized value.</param>
+    protected abstract void Deserialize(in BinarySpanReader binaryReader, ref T result);
+
     int IBinaryWriter<T>.Serialize(in T data, Span<byte> buffer)
     {
         var offset = 0;
@@ -25,6 +69,7 @@ public abstract class BinarySerializer<T> : IBinarySerializer<T>
         Serialize(in writer, in data);
         return offset;
     }
+
     int IBinaryReader<T>.Deserialize(ReadOnlySpan<byte> data, ref T value)
     {
         var offset = 0;
