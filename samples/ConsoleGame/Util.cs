9d783126513b87754d3d51038b814430c878ae51
Lucas Teles <lucas.teles@outlook.com>
2024-05-23 18:33:53 -0300

    update samples

--- samples/ConsoleGame/Util.cs
+++ samples/ConsoleGame/Util.cs
@@ -1,17 +1,7 @@
 using Backdash.Serialization;
-using Backdash.Serialization.Buffer;
+
 namespace ConsoleGame;
 
-public sealed class MyStateSerializer : BinarySerializer<GameState>
-{
-    protected override void Serialize(in BinarySpanWriter binaryWriter, in GameState data)
-    {
-        binaryWriter.Write(data.Position1);
-        binaryWriter.Write(data.Position2);
-    }
-    protected override void Deserialize(in BinarySpanReader binaryReader, ref GameState result)
-    {
-        result.Position1 = binaryReader.ReadVector2();
-        result.Position2 = binaryReader.ReadVector2();
-    }
-}
+[StateSerializer<GameState>]
+public partial class GameStateSerializer;
+
