85353ca2e9e6a3acc94cb72b35aa01d86b60a35f
lucasteles <lucasteles@users.noreply.github.com>
2025-06-19 21:03:09 +0000

    deploy: 4637ec8818821088f57ced9b061b4990b986d398

---
+++ public/main.js
@@ -0,0 +1,10 @@
+﻿export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/lucasteles/Backdash',
+            title: 'GitHub'
+        }
+    ]
+}
